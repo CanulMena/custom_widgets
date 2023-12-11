a3f83f34e172309124c75d819de507d1b379c187
Gustavo Canul <gusssy12@gmail.com>
2023-12-11 12:53:45 -0600

    _HomeView creando y implementado en el body del  HomeScreen

--- lib/config/menu/menu_item.dart
+++ lib/config/menu/menu_item.dart
@@ -6,27 +6,28 @@ class MenuItem { //* Esta esta clase solo nos ayudará a controlar los MenuItems
   final String link;
   final IconData icon;
 
-  MenuItem({
+  const MenuItem({
   required this.title, 
   required this.subTitle, 
   required this.link, 
   required this.icon
   });
-
-  List<MenuItem> appMenuItems = [
-    MenuItem(
-      title: 'botones', 
-      subTitle: 'varios botones en flutter', 
-      link: '/botones', 
-      icon: Icons.smart_button_outlined
-      ),
-    MenuItem(
-      title: 'tarjetas', 
-      subTitle: 'un contenedor estilizado', 
-      link: '/tarjetas', 
-      icon: Icons.credit_card
-      ),
-    
-  ];
-  
 }
+
+//!Al no estar dentro de una clase podemos acceder a esta variable con el simple echo de importar el enlace de este documento(codigo)
+const List<MenuItem> appMenuItems =[
+  
+MenuItem(
+  title: 'botones', 
+  subTitle: 'varios botones en flutter', 
+  link: '/botones', 
+  icon: Icons.smart_button_outlined
+  ),
+MenuItem(
+  title: 'tarjetas', 
+  subTitle: 'un contenedor estilizado', 
+  link: '/tarjetas', 
+  icon: Icons.credit_card
+  ),
+
+];
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:custom_widgets/config/menu/menu_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,9 +10,45 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material3'),
       ),
-      body: Center(
-          child: FilledButton(onPressed: () {}, child: const Text('Hola mundo')),
-        ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final MenuItem menuItem = appMenuItems[index];
+        return CustomListTitle(menuItem: menuItem);
+      },
+      );
+  }
+}
+
+class CustomListTitle extends StatelessWidget {
+  final MenuItem menuItem;
+
+  const CustomListTitle({
+    super.key,
+    required this.menuItem,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        //TODO: navegar a otra pantalla
+      },
     );
   }
 }
